a0116d221df53103c58fbc9e37808e30ed14ad94
jakub tomal <jakub.tomal@smcebi.edu.pl>
2019-05-16 22:34:58 +0200

    Dodane przełącznie się między oknami do logowania i rejestracji

--- app/src/main/java/com/example/komunikator/LoginActivity.java
+++ app/src/main/java/com/example/komunikator/LoginActivity.java
@@ -3,17 +3,44 @@ package com.example.komunikator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button loginButton,phoneLoginButton;
+    private EditText userEmail,userPassword;
+    private TextView needNewAccountLink,forgetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeFields();
+
+        needNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
+            }
+        });
+    }
+
+    private void InitializeFields() {
+        loginButton = (Button) findViewById(R.id.login_button);
+        phoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+
+        userEmail = (EditText) findViewById(R.id.login_email);
+        userPassword = (EditText) findViewById(R.id.login_password);
+
+        needNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+        forgetPassword = (TextView) findViewById(R.id.forget_password_link);
     }
 
     @Override
@@ -22,13 +49,19 @@ public class LoginActivity extends AppCompatActivity {
 
         if(currentUser != null)
         {
-            sendUserToMainActivity();
+            SendUserToMainActivity();
         }
     }
 
-    private void sendUserToMainActivity() {
+    private void SendUserToMainActivity() {
 
         Intent loginIntent = new Intent(LoginActivity.this , MainActivity.class);
+        startActivity(loginIntent);
+    }
+
+    private void SendUserToRegisterActivity() {
+
+        Intent loginIntent = new Intent(LoginActivity.this , RegisterActivity.class);
         startActivity(loginIntent);
     }
 }
